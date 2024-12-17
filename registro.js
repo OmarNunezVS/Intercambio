515dbc5016253f91379777e9554b5329d5a02cfe
Omar Nuñez <OmarNnzV@gmail.com>
2024-12-16 23:07:35 -0700

    INIT

---
+++ registro.js
@@ -0,0 +1,8 @@
+$("#btnRegistro").click(function () {
+    Swal.fire({
+        title: "¡Éxito!",
+        text: "Se ha registrado con éxito.",
+        icon: "success",
+        timer: 3000
+    });
+});
